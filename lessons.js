937bea8df9eabf0cf52385028b7a6de45e8d7f62
Brittney <brittfort34@gmail.com>
2018-07-26 05:31:12 +0000

    Version 6

--- lessons.js
+++ lessons.js
@@ -35,6 +35,30 @@ var todoList = {
     var todo = this.todos[position];
     todo.completed = !todo.completed;
     this.displayTodos();
+  },
+  toggleAll: function() {
+    var totalTodos = this.todos.length;
+    var completedTodos = 0;
+    
+    // Get number of completed Todos
+    for (var i = 0; i < totalTodos; i++) {
+      if (this.todos[i].completed === true) {
+        completedTodos++;
+      }
+    }
+    // Case 2: If everything is true, make everything false
+    if (completedTodos === totalTodos) {
+      // Make everything false
+      for (var i = 0; i < totalTodos; i++) {
+       this.todos[i].completed = false; 
+      }
+    // Case 2: Otherwise, make everything true  
+    } else {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = true;
+      }
+    }
+    this.displayTodos();
   }
 };
 
